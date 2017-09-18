6b8fa35418c849c7e1345eddb687f2d19cb5765d
ThuyHien261195 <thuyhien261195@gmail.com>
2017-09-18 15:03:50 +0700

    Add landscape recycler view holder

---
+++ app/src/main/java/com/hasbrain/areyouandroiddev/adapter/LandscapePostViewHolder.java
@@ -0,0 +1,28 @@
+package com.hasbrain.areyouandroiddev.adapter;
+
+import android.content.Context;
+import android.view.View;
+
+import com.hasbrain.areyouandroiddev.model.RedditPost;
+
+import java.util.List;
+
+/**
+ * Created by thuyhien on 9/18/17.
+ */
+
+public class LandscapePostViewHolder extends PostViewHolder {
+
+    public LandscapePostViewHolder(View view){
+        super(view);
+    }
+
+    @Override
+    public void bindContentPostView(Context context,
+                                    List<String> titleList,
+                                    List<Integer> colorTitleList,
+                                    RedditPost redditPost) {
+        super.bindContentPostView(context, titleList, colorTitleList, redditPost);
+        textViewAuthor.setText(redditPost.getAuthor());
+    }
+}
