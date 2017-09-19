d2149b45f95d3eeeb27033836e31db5bca0886a5
ThuyHien261195 <thuyhien261195@gmail.com>
2017-09-20 01:09:18 +0700

    Add function create time title list and update function getPostTime

--- app/src/main/java/com/hasbrain/areyouandroiddev/FormatStringUtil.java
+++ app/src/main/java/com/hasbrain/areyouandroiddev/FormatStringUtil.java
@@ -2,12 +2,13 @@ package com.hasbrain.areyouandroiddev;
 
 import android.content.Context;
 import android.os.Build;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.content.ParallelExecutorCompat;
 import android.text.Html;
 import android.text.Spanned;
 
-import java.util.Calendar;
+import com.hasbrain.areyouandroiddev.model.RedditPost;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -15,6 +16,15 @@ import java.util.List;
  */
 
 public class FormatStringUtil {
+    public static final long MILLISECS_PER_YEAR = 1000L * 60 * 60 * 24 * 365;
+    public static final long MILLISECS_PER_MONTH = 1000L * 60 * 60 * 24 * 30;
+    public static final long MILLISECS_PER_DAY = 1000L * 60 * 60 * 24;
+    public static final long MILLISECS_PER_HOUR = 1000L * 60 * 60;
+    public static final String YEAR_TITLE = "year";
+    public static final String MONTH_TITLE = "month";
+    public static final String DAY_TITLE = "day";
+    public static final String HOUR_TITLE = "hour";
+
     @SuppressWarnings("deprecation")
     public static Spanned fromHtml(String author) {
         Spanned result;
@@ -31,34 +41,48 @@ public class FormatStringUtil {
         return "<font color='" + hexColor + "'>" + text + "</font>";
     }
 
-    public static String formatAuthorTitle(String authorTitle, String author, String subReddit, int color) {
+    public static String formatAuthorTitle(
+            String authorTitle, String author, String subReddit, int color) {
         author = getColoredSpanned(author, color);
         subReddit = getColoredSpanned(subReddit, color);
         return String.format(authorTitle, author, subReddit);
     }
 
-    public static String getPostTime(long createdUTC, List<String> titleList) {
+    public static String getPostTime(long createdUTC, HashMap<String, String> timeTitleList) {
         long currentTime = System.currentTimeMillis();
         long betweenTime = currentTime - createdUTC * 1000L;
-        int postTime = (int) (betweenTime / ConstantCollection.MILLISECS_PER_YEAR);
+        int postTime = (int) (betweenTime / MILLISECS_PER_YEAR);
         if (postTime != 0) {
-            return String.format(titleList.get(2), postTime);
+            return String.format(timeTitleList.get(YEAR_TITLE), postTime);
         }
 
-        postTime = (int) (betweenTime / ConstantCollection.MILLISECS_PER_MONTH);
+        postTime = (int) (betweenTime / MILLISECS_PER_MONTH);
         if (postTime != 0) {
-            return String.format(titleList.get(3), postTime);
+            return String.format(timeTitleList.get(MONTH_TITLE), postTime);
         }
 
-        postTime = (int) (betweenTime / ConstantCollection.MILLISECS_PER_DAY);
+        postTime = (int) (betweenTime / MILLISECS_PER_DAY);
         if (postTime != 0) {
-            return String.format(titleList.get(4), postTime);
+            return String.format(timeTitleList.get(DAY_TITLE), postTime);
         }
 
-        postTime = (int) (betweenTime / ConstantCollection.MILLISECS_PER_HOUR);
+        postTime = (int) (betweenTime / MILLISECS_PER_HOUR);
         if (postTime != 0) {
-            return String.format(titleList.get(5), postTime);
+            return String.format(timeTitleList.get(HOUR_TITLE), postTime);
         }
         return "";
+    }
+
+    public static HashMap<String, String> createTimeTitleList(Context context) {
+        HashMap<String, String> titleList = new HashMap<>();
+        titleList.put(YEAR_TITLE,
+                context.getResources().getString(R.string.title_created_years));
+        titleList.put(MONTH_TITLE,
+                context.getResources().getString(R.string.title_created_months));
+        titleList.put(DAY_TITLE,
+                context.getResources().getString(R.string.title_created_days));
+        titleList.put(HOUR_TITLE,
+                context.getResources().getString(R.string.title_created_hours));
+        return titleList;
     }
 }
