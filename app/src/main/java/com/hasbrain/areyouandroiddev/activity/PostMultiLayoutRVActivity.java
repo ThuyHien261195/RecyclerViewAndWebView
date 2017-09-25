b6c4dc8a57d61e93843a7a0af35cda56e1714678
ThuyHien261195 <thuyhien261195@gmail.com>
2017-09-25 15:11:45 +0700

    Add Multiple Layout Recycler View Holder, Adapter, Activity class and delete style in values-land

---
+++ app/src/main/java/com/hasbrain/areyouandroiddev/activity/PostMultiLayoutRVActivity.java
@@ -0,0 +1,8 @@
+package com.hasbrain.areyouandroiddev.activity;
+
+/**
+ * Created by thuyhien on 9/25/17.
+ */
+
+public class PostMultiLayoutRVActivity {
+}
---
+++ app/src/main/java/com/hasbrain/areyouandroiddev/adapter/MultiLayoutRVAdapter.java
@@ -0,0 +1,8 @@
+package com.hasbrain.areyouandroiddev.adapter;
+
+/**
+ * Created by thuyhien on 9/25/17.
+ */
+
+public class MultiLayoutRVAdapter {
+}
